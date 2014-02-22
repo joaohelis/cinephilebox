cbf9235ab28bb44a21585fa49373e932ca714171
Smith Ascari <smithascari@gmail.com>
2014-02-22 00:40:07 -0300

    add validation to category form

--- application/views/admin/category_form.js
+++ application/views/admin/category_form.js
@@ -8,6 +8,37 @@ window.CategoryFormView = Backbone.View.extend({
     render: function () {
         $(this.el).html(this.template(this.model.toJSON()));
         return this;
-    }
+    },
+
+    events: {
+        "change"                  : "change",
+        "click #category-save"    : "beforeSave",               
+    },
+
+    change: function (event) {
+        common_functions.change(event, this.model);
+    },
+
+    beforeSave: function () {        
+        var check = utils.validateAll(this.model);
+        if (check.isValid === false){            
+            utils.displayValidationErrors(check.messages);
+            return false;
+        }
+        this.saveCategory();
+        return false;                
+    },
+
+    saveCategory:function (){        
+        if (this.model.isNew()){
+            categoryList.create(this.model);
+            alert('Categoria criada com sucesso!');
+            window.location.replace('#admin');      ;
+        } else {            
+            this.model.save();
+            utils.showAlert('Sucesso!', 'Suas alterações foram salvas.', 'alert-success');
+        }
+        return false;
+    },
 
 });
